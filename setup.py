468f590e45da2928553aa87cb430a4e9a6980238
Javier-Jimenez99 <javijj99@gmail.com>
2024-05-29 00:00:59 +0000

    chore: Update Python version to 3.9.19 and add dependencies to env.yml and requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="gatherer_sage",
+    use_scm_version=True,
+    author="Javier Jimenez",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        "pandas",
+        "langchain",
+        "transformers",
+        "PyPDF2",
+        "matplotlib",
+        "torch",
+        "huggingface_hub",
+        "langchain_community",
+        "sentence-transformers",
+        "faiss-gpu",
+        "bitsandbytes",
+        "accelerate",
+        "ragatouille"
+    ],
+)
