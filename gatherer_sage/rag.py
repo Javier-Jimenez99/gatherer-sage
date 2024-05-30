1787e0637c51ee186e8878ea456c0666e64f6517
Javier-Jimenez99 <javijj99@gmail.com>
2024-05-30 09:42:57 +0000

    add store db

--- gatherer_sage/rag.py
+++ gatherer_sage/rag.py
@@ -13,44 +13,16 @@ from gatherer_sage.utils import get_pdf_content, load_cards_df, card_texts
 class RAG:
     def __init__(
         self,
+        vector_database_path: str = None,
         rules_folder: str = "data/rules/",
         cards_path="data/AtomicCards.json",
         embedding_model_path="thenlper/gte-large",
         chunk_size: int = 512,
         reranker_model_path="colbert-ir/colbertv2.0",
     ):
-        self.chunk_size = chunk_size
-        self.embedding_model_path = embedding_model_path
-        self.reranker_model_path = reranker_model_path
 
-        doc_paths = [rules_folder + f for f in os.listdir(rules_folder)]
-        self.langchaing_docs = [
-            LangchainDocument(doc) for doc in get_pdf_content(doc_paths)
-        ]
-
-        cards_df = load_cards_df(cards_path)
-
-        all_cards_texts = card_texts(cards_df)
-
-        cards_langchain_docs = [LangchainDocument(doc) for doc in all_cards_texts]
-        self.langchaing_docs.extend(cards_langchain_docs)
-
-        self.markdown_separators = [
-            "\n#{1,6} ",
-            "```\n",
-            "\n\\*\\*\\*+\n",
-            "\n---+\n",
-            "\n___+\n",
-            "\n\n",
-            "\n",
-            " ",
-            "",
-        ]
-
-        self.docs_processed = self.split_documents()
-
-        self.embedding_model = HuggingFaceEmbeddings(
-            model_name=self.embedding_model_path,
+        embedding_model = HuggingFaceEmbeddings(
+            model_name=embedding_model_path,
             multi_process=True,
             model_kwargs={"device": "cuda"},
             encode_kwargs={
@@ -58,41 +30,81 @@ class RAG:
             },  # Set `True` for cosine similarity
         )
 
-        self.vector_database = FAISS.from_documents(
-            self.docs_processed,
-            self.embedding_model,
-            distance_strategy=DistanceStrategy.COSINE,
-        )
+        self.reranker = RAGPretrainedModel.from_pretrained(reranker_model_path)
 
-        self.reranker = RAGPretrainedModel.from_pretrained(self.reranker_model_path)
+        if vector_database_path is None:
 
-    def split_documents(self):
-        """
-        Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
-        """
+            def _split_documents():
+                """
+                Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
+                """
 
-        text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-            AutoTokenizer.from_pretrained(self.embedding_model_path),
-            chunk_size=self.chunk_size,
-            chunk_overlap=int(self.chunk_size / 10),
-            add_start_index=True,
-            strip_whitespace=True,
-            separators=self.markdown_separators,
-        )
+                text_splitter = (
+                    RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+                        AutoTokenizer.from_pretrained(embedding_model_path),
+                        chunk_size=chunk_size,
+                        chunk_overlap=int(chunk_size / 10),
+                        add_start_index=True,
+                        strip_whitespace=True,
+                        separators=markdown_separators,
+                    )
+                )
 
-        docs_processed = []
-        for doc in self.langchaing_docs:
-            docs_processed += text_splitter.split_documents([doc])
+                docs_processed = []
+                for doc in langchaing_docs:
+                    docs_processed += text_splitter.split_documents([doc])
 
-        # Remove duplicates
-        unique_texts = {}
-        docs_processed_unique = []
-        for doc in docs_processed:
-            if doc.page_content not in unique_texts:
-                unique_texts[doc.page_content] = True
-                docs_processed_unique.append(doc)
+                # Remove duplicates
+                unique_texts = {}
+                docs_processed_unique = []
+                for doc in docs_processed:
+                    if doc.page_content not in unique_texts:
+                        unique_texts[doc.page_content] = True
+                        docs_processed_unique.append(doc)
 
-        return docs_processed_unique
+                return docs_processed_unique
+
+            doc_paths = [rules_folder + f for f in os.listdir(rules_folder)]
+            langchaing_docs = [
+                LangchainDocument(doc) for doc in get_pdf_content(doc_paths)
+            ]
+
+            cards_df = load_cards_df(cards_path)
+
+            all_cards_texts = card_texts(cards_df)
+
+            cards_langchain_docs = [LangchainDocument(doc) for doc in all_cards_texts]
+            langchaing_docs.extend(cards_langchain_docs)
+
+            markdown_separators = [
+                "\n#{1,6} ",
+                "```\n",
+                "\n\\*\\*\\*+\n",
+                "\n---+\n",
+                "\n___+\n",
+                "\n\n",
+                "\n",
+                " ",
+                "",
+            ]
+
+            docs_processed = _split_documents()
+
+            self.vector_database = FAISS.from_documents(
+                docs_processed,
+                embedding_model,
+                distance_strategy=DistanceStrategy.COSINE,
+            )
+        else:
+            self.vector_database = FAISS.load_local(
+                vector_database_path,
+                embedding_model,
+                distance_strategy=DistanceStrategy.COSINE,
+                allow_dangerous_deserialization=True,
+            )
+
+    def store_db(self, vector_database_path: str):
+        self.vector_database.save_local(vector_database_path)
 
     def retrieve_context(
         self, question: str, num_retrieved_docs: int = 30, num_docs_final: int = 5
